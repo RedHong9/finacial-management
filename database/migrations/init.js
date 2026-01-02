ef86fb5849b11b48ae707780e5c29f239f0d78f5
RedHong9 <r1305238144@outlook.com>
2026-01-02 15:50:11 +0800

    feat: 上传数据库迁移脚本

---
+++ database/migrations/init.js
@@ -0,0 +1,10 @@
+/**
+ * ���ݿ�Ǩ�ƽű�
+ * ���д˽ű��Դ�����������ݿ����
+ */
+
+const { initDatabase } = require('../../config/database');
+
+console.log('��ʼ���ݿ�Ǩ��...');
+initDatabase();
+console.log('���ݿ�Ǩ����ɡ�');
